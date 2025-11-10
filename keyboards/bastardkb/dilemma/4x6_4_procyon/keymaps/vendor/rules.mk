b6705c383838a693a77f11fb51324cb66822391c
Emeku <ivertroxmt@gmail.com>
2025-11-10 01:41:53 +0100

    Update vendor keymap

--- keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/vendor/rules.mk
+++ keyboards/bastardkb/dilemma/4x6_4_procyon/keymaps/vendor/rules.mk
@@ -1,12 +1,7 @@
 VIA_ENABLE = yes
 ENCODER_MAP_ENABLE = yesç
 
-# Asegura el pointing en split
 POINTING_DEVICE_ENABLE = yes
 SPLIT_POINTING_ENABLE = yes
 
-# (Opcional, suele venir por el teclado Procyon)
-# POINTING_DEVICE_DRIVER = cirque_pinnacle_spi
-
-# Para que existan los keycodes MS_* (MS_BTN1, MS_WHLU, etc.)
 MOUSEKEY_ENABLE = yes
